e9a257c41fc1aadd106081e8c71c440f14296617
augusto-m-r <augusto.mora.rodrigues@escola.pr.gov.br>
2026-05-07 10:58:36 -0300

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'LojaCarros'
+});
+
+app.get('/', (req, res) => {
+    res.send('API Funcionando');
+})
+
+app.post('/carros', (req, res) => {
+    const{titulo, preco, marca, modelo,cambio} =
+    req.body;
+    const sql = `INSERT INTO carros (titulo, preco, marca, modelo, cambio) VALUES (?, ?, ?, ?, ?)`;
+
+    db.query(sql, [titulo, preco, marca, modelo, cambio], (err, result) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Erro');
+        } else {
+            res.status(201).send(result);
+        }
+    });
+});
